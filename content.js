151a84bceb43c30a9ebdf2f7049ae45120f501f6
Rajilesh Panoli <rajileshpanoli123@gmail.com>
2021-05-22 12:46:15 +0530

    Updated the code

--- content.js
+++ content.js
@@ -12,15 +12,36 @@ height: 100vh;
 z-index: 9999999;`;
 },1000);
 
+var paramsString = window.location.search;
+var searchParams = new URLSearchParams(paramsString);
+
+
+if(searchParams.has("show_chart") == false){
+
+
+  setTimeout(function(){
+    document.getElementById('app').style.opacity = 0;
+    document.getElementById('app').style.pointerEvents = 'none';
+  },1000);
+  
+}
+
 
 window.addEventListener("keydown", (e) => {
+
+
     if (e.key == "q") {
-        document.getElementById('app').style.display = document.getElementById('app').style.display =='none' ? 'block' : 'none';
+      document.getElementById('app').style.opacity = 1;
+      document.getElementById('app').style.pointerEvents = 'all';
+        document.getElementById('app').style.display = document.getElementById('app').style.display !='none' ? 'none' : 'block';
+        document.getElementById('app').style.background = '#fff';
     //   th.autoFetch();
     //   this.history_active ='';
     }else if (e.key == "w") {
-      document.getElementById('app').style.display = document.getElementById('app').style.display =='none' ? 'block' : 'block';
-      document.getElementById('app').style.background = document.getElementById('app').style.background =='none' ? '#fff' : 'none';
+      document.getElementById('app').style.opacity = 1;
+      document.getElementById('app').style.pointerEvents = 'all';
+      document.getElementById('app').style.display = document.getElementById('app').style.display !='none' ? 'block' : 'block';
+      document.getElementById('app').style.background = document.getElementById('app').style.background !='none' ? 'none' : '#fff';
   //   th.autoFetch();
   //   this.history_active ='';
   }else if(e.key){
